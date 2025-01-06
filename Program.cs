335c16480373b70b81e259109f08a12ad3768de8
JuliaCarew <crewjulia3@gmail.com>
2025-01-06 12:27:57 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliaC_Properties___Encapsulation_jan062025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class GameCharacter
+    {
+        // 1. Auto-implemented property
+        public string Name { get; private set; }
+
+        // 2. Full property with validation
+        private int health;
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                // TODO: Validate between 0-100
+            }
+        }
+
+        // 3. Computed property
+        public bool IsAlive
+        {
+            get
+            {
+                // TODO: Return true if health > 0
+            }
+        }
+    }
+
+    public class Inventory
+    {
+        // TODO: Implement properties as specified
+    }
+
+    public class PowerUp
+    {
+        // TODO: Implement properties as specified
+    }
+}
